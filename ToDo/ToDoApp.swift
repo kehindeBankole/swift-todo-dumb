92dfe5054084da7d3fd10a311ffd84f223e73b1f
kehindeBankole <bankolek1@gmail.com>
2023-08-25 06:19:38 +0100

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by kehinde on 25/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
